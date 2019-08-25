84980b9582256655cc8997e6664bd50017320b0d
Seflaks <seflaks@gmail.com>
2019-08-25 20:13:16 +0300

    add JUnit

---
+++ test/ru/seflaks/basejava/storage/AbstractArrayStorageTest.java
@@ -0,0 +1,11 @@
+package ru.seflaks.basejava.storage;
+
+public abstract class AbstractArrayStorageTest {
+    private Storage storage;
+
+    private static final String UUID_1 = "uuid1";
+    private static final String UUID_2 = "uuid2";
+    private static final String UUID_3 = "uuid3";
+
+
+}
