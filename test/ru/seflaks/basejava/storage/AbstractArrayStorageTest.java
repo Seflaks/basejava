9a58b308d57dfadd72e626160aeb3988df8ed412
Seflaks <seflaks@gmail.com>
2019-08-26 09:15:48 +0300

    add JUnit2 and refactoring

--- test/ru/seflaks/basejava/storage/AbstractArrayStorageTest.java
+++ test/ru/seflaks/basejava/storage/AbstractArrayStorageTest.java
@@ -1,11 +1,59 @@
 package ru.seflaks.basejava.storage;
 
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import ru.seflaks.basejava.exception.NotExistStorageException;
+import ru.seflaks.basejava.model.Resume;
+
 public abstract class AbstractArrayStorageTest {
-    private Storage storage;
+    private Storage storage = new ArrayStorage();
 
     private static final String UUID_1 = "uuid1";
     private static final String UUID_2 = "uuid2";
     private static final String UUID_3 = "uuid3";
 
+    @Before
+    public void setUp() throws Exception {
+
+        storage.clear();
+        storage.save(new Resume(UUID_1));
+        storage.save(new Resume(UUID_2));
+        storage.save(new Resume(UUID_3));
+    }
+
+    @Test
+    public void clear() {
+    }
+
+    @Test
+    public void update() {
+    }
+
+    @Test
+    public void getAll() {
+    }
+
+    @Test
+    public void save() {
+    }
+
+    @Test
+    public void delete() {
+    }
+
+    @Test
+    public void get() {
+    }
+
+    @Test
+    public void size() {
+        Assert.assertEquals(3, storage.size());
+    }
+
+    @Test(expected = NotExistStorageException.class)
+    public void getNotExist() {
+        storage.get("dummy");
+    }
 
 }
