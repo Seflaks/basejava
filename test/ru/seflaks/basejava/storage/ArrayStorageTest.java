5b2f96670f8051221479e1ae8444dfc51cc5dffc
Seflaks <seflaks@gmail.com>
2019-08-26 20:03:04 +0300

    HW04 implementation Tests Abstract and children

--- test/ru/seflaks/basejava/storage/ArrayStorageTest.java
+++ test/ru/seflaks/basejava/storage/ArrayStorageTest.java
@@ -3,5 +3,7 @@ package ru.seflaks.basejava.storage;
 import static org.junit.Assert.*;
 
 public class ArrayStorageTest extends AbstractArrayStorageTest{
-
+    public ArrayStorageTest() {
+        super(new ArrayStorage());
+    }
 }
